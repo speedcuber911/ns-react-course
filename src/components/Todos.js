17a06afe5417bd6e76ac262b53f1334cfe417d60
Parikshit <parikshit.joon@gmail.com>
2023-03-12 12:39:44 +0530

    todo app project

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,80 +1,163 @@
 import { useState, useReducer } from "react";
-// function ToDo(props) {
-//   return (
-//     <div>
-//       <label>
-//         <input
-//           type="checkbox"
-//           checked={props.status}
-//           onChange={(event) => console.log(event.target)}
-//         />
-//         {props.title}
-//       </label>
-//     </div>
-//   );
-// }
 
-const tasks = [
-  { title: "Task1", id: 1, status: true },
-  { title: "Task2", id: 2, status: false },
-  { title: "Task3", id: 3, status: true },
-  { title: "Task4", id: 4, status: false },
-];
+/*
+
+  A To-do app
+  1. Capability of adding tasks
+  2. Capability of removing tasks
+  3. Capability of editting tasks
+  4. Filter for (complete/incomplete) tasks
+  5. Date and time of when the task is added & when the task was last editted
+  6. Styling
+
+
+*/
+
+function getFormattedDate() {
+  const t = new Date();
+  return t.toDateString() + "|" + t.toTimeString().split(" ")[0];
+}
+
+function ToDoItem(props) {
+  return (
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          checked={props.status}
+          onChange={() => {
+            // handleChange(task.id);
+            props.dispatcher({
+              // Dispatcher, dispactches action and calls the reducer with these actions
+              type: "DONE",
+              id: props.id,
+              lastUpdated: getFormattedDate(),
+            });
+          }}
+        />
+        <span style={{ textDecoration: props.status ? "line-through" : "" }}>
+          {props.title}
+        </span>
+        <span style={{ fontSize: "10px" }}>({props.createdAt})</span>
+        {props.lastUpdated ? (
+          <span style={{ fontSize: "10px" }}>({props.lastUpdated})</span>
+        ) : (
+          <></>
+        )}
+      </label>
+      <div
+        onClick={() => {
+          props.dispatcher({
+            type: "REMOVE_ITEM",
+            id: props.id,
+          });
+        }}
+        style={{
+          display: "inline-block",
+          color: "red",
+          fontSize: "10px",
+          cursor: "pointer",
+        }}
+      >
+        X
+      </div>
+    </div>
+  );
+}
 
 function Todos(props) {
+  // Pure function. When called with same input, always same output
+  // Don't cause side-effects
   const reducer = (state, action) => {
+    // Always returns a new state. Don't mutate the state
+    console.log("Reducer called");
     // Whatever the reducer returns, becomes the new state
     if (action.type === "DONE") {
       return state.map((task) => {
         if (task.id === action.id) {
-          return { ...task, status: !task.status };
+          return {
+            ...task,
+            status: !task.status,
+            lastUpdated: action.lastUpdated,
+          };
         } else return task;
       });
+    }
+    if (action.type === "ADD_ITEM") {
+      if (state.find((el) => el.id === action.id)) {
+        return state;
+      } else {
+        return [
+          ...state,
+          {
+            title: action.title,
+            id: action.id,
+            status: false,
+            createdAt: action.createdAt,
+          },
+        ];
+      }
+    }
+    if (action.type === "REMOVE_ITEM") {
+      return state.filter((el) => action.id !== el.id);
     }
     return state;
   };
 
   //   const [taskStatus, setTask] = useState(tasks);
-  const [taskStatus2, dispatcher] = useReducer(reducer, tasks);
-
-  const handleChange = (taskId) => {
-    // taskStatus.forEach((task) => {
-    //   if (task.id === taskId) {
-    //     task.status = !task.status;
-    //   }
-    // });
-    // console.log({ taskStatus });
-    // setTask([...taskStatus]);
-    // setTask(
-    //   taskStatus.map((task) => {
-    //     if (task.id === taskId) {
-    //       return { ...task, status: !task.status };
-    //     } else return task;
-    //   })
-    // );
-  };
+  const [taskStatus2, dispatcher] = useReducer(reducer, []);
+  const [taskValue, setTaskValue] = useState("");
+  const [filterState, setFilterState] = useState("All");
 
   return (
     <>
-      {taskStatus2.map((task) => (
-        <div key={task.id}>
-          <label>
-            <input
-              type="checkbox"
-              checked={task.status}
-              onChange={() => {
-                // handleChange(task.id);
-                dispatcher({
-                  // Dispatcher, dispactches action and calls the reducer with these actions
-                  type: "DONE",
-                  id: task.id,
-                });
-              }}
-            />
-            {task.title}
-          </label>
-        </div>
-      ))}
+      <select
+        onChange={(event) => setFilterState(event.target.value)}
+        value={filterState}
+      >
+        <option>All</option>
+        <option>Complete</option>
+        <option>In-complete</option>
+      </select>
+      {taskStatus2
+        .filter((taskEl) => {
+          if (filterState === "Complete" && taskEl.status) {
+            return true;
+          } else if (filterState === "In-complete" && !taskEl.status) {
+            return true;
+          } else if (filterState === "All") return true;
+        })
+        .map((task) => (
+          <ToDoItem
+            status={task.status}
+            id={task.id}
+            key={task.id}
+            title={task.title}
+            dispatcher={dispatcher}
+            createdAt={task.createdAt}
+            lastUpdated={task.lastUpdated}
+          />
+        ))}
+      <input
+        type="text"
+        placeholder="Task description"
+        value={taskValue}
+        onChange={(event) => setTaskValue(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          console.log("Clicked on add task");
+          dispatcher({
+            type: "ADD_ITEM",
+            title: taskValue,
+            id: taskStatus2.length + 1,
+            createdAt: getFormattedDate(),
+          });
+          setTaskValue("");
+        }}
+      >
+        Add task
+      </button>
     </>
   );
 }
